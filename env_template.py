dd0340f59d3a6241ba8c2daaf3b906b25dda9fc0
Liam Jackson <Ljackson3613@yahoo.com>
2021-06-28 14:34:08 -0500

    Added acquisition folder to utilities repo

---
+++ env_template.py
@@ -0,0 +1,24 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Private info
+
+# IP adress for the server
+host = 
+# Username
+user = 
+# Password
+password =
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Use this to get into SQL db
+
+# import pandas as pd
+# import env
+
+# def get_connection(db, user=env.user, host=env.host, password=env.password):
+    # return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+# df = pd.read_sql('SELECT * FROM properties_2017', get_connection('zillow'))
+
+# df.head(5)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
